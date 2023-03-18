73600efe5c83f60814a2c771ffdb79f37e823ebf
Kiyan Yang <KiyanYang@outlook.com>
2023-03-18 12:35:45 +0800

    MouseOver的动画效果

--- WPF.Demo.Navigation/Behaviors/SlidingNavigationBehavior.cs
+++ WPF.Demo.Navigation/Behaviors/SlidingNavigationBehavior.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Xaml.Behaviors;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
@@ -8,106 +7,126 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 
+using Microsoft.Xaml.Behaviors;
+
 namespace WPF.Demo.Navigation.Behaviors;
 
 internal class SlidingNavigationBehavior : Behavior<FrameworkElement>
 {
-    private FrameworkElement sliding = new();
+	private FrameworkElement sliding = new();
 
-    public string SlidingName
-    {
-        get { return (string)GetValue(SlidingNameProperty); }
-        set { SetValue(SlidingNameProperty, value); }
-    }
+	public string SlidingName
+	{
+		get { return (string)GetValue(SlidingNameProperty); }
+		set { SetValue(SlidingNameProperty, value); }
+	}
 
-    public static readonly DependencyProperty SlidingNameProperty = DependencyProperty.Register(
-        nameof(SlidingName),
-        typeof(string),
-        typeof(SlidingNavigationBehavior)
-    );
+	public static readonly DependencyProperty SlidingNameProperty = DependencyProperty.Register(
+		nameof(SlidingName),
+		typeof(string),
+		typeof(SlidingNavigationBehavior)
+	);
 
-    private IList<RadioButton> Children { get; set; }
+	private IList<RadioButton> Children { get; set; }
 
-    private FrameworkElement prev { get; set; }
+	protected override void OnAttached()
+	{
+		AssociatedObject.Loaded += (_, _) =>
+		{
+			sliding = (FrameworkElement)AssociatedObject.FindName(SlidingName);
+			Children = GetChildren<RadioButton>().ToList();
+			foreach (var child in Children)
+			{
+				child.MouseEnter += Child_MouseEnter;
+				child.MouseLeave += Child_MouseLeave;
+				if (child.IsChecked == true)
+				{
+					InitSliding(child);
+				}
+			}
+		};
+	}
 
-    protected override void OnAttached()
-    {
-        AssociatedObject.Loaded += (_, _) =>
-        {
-            sliding = (FrameworkElement)AssociatedObject.FindName(SlidingName);
-            Children = GetChildren<RadioButton>().ToList();
-            foreach (var child in Children)
-            {
-                child.Checked += OnRadioButtonChecked;
-                if ((bool)child.IsChecked)
-                {
-                    BeginAnimation(child);
-                }
-            }
-        };
-    }
+	private void Child_MouseEnter(object sender, MouseEventArgs e)
+	{
+		if (sender is FrameworkElement el)
+		{
+			BeginAnimation(el);
+		}
+	}
 
-    private void OnRadioButtonChecked(object sender, RoutedEventArgs e)
-    {
-        BeginAnimation(sender as FrameworkElement);
-    }
+	private void Child_MouseLeave(object sender, MouseEventArgs e)
+	{
+		var el = Children.FirstOrDefault(x => x.IsChecked == true);
+		if (el is not null)
+		{
+			BeginAnimation(el);
+		}
+	}
 
-    private void BeginAnimation(FrameworkElement e)
-    {
-        var offset = VisualTreeHelper.GetOffset(e);
-        if (sliding.Width > 0 && sliding.Height > 0)
-        {
-            var storyboard = new Storyboard();
-            var easingFuction = new PowerEase { EasingMode = EasingMode.EaseOut, };
+	private void InitSliding(FrameworkElement e)
+	{
+		var p = GetPosition(e);
+		sliding.Width = p.ActualWidth;
+		sliding.Height = p.ActualHeight;
+		sliding.RenderTransform = new TranslateTransform(p.OffSetX, p.OffSetY);
+	}
 
-            AddStoryboard(storyboard, easingFuction, e.ActualWidth, "Width");
-            AddStoryboard(storyboard, easingFuction, e.ActualHeight, "Height");
-            AddStoryboard(
-                storyboard,
-                easingFuction,
-                offset.X,
-                "(UIElement.RenderTransform).(TranslateTransform.X)"
-            );
-            AddStoryboard(
-                storyboard,
-                easingFuction,
-                offset.Y,
-                "(UIElement.RenderTransform).(TranslateTransform.Y)"
-            );
+	private void BeginAnimation(FrameworkElement e)
+	{
+		var position = GetPosition(e);
+		var storyboard = new Storyboard();
+		var easingFuction = new PowerEase { EasingMode = EasingMode.EaseOut, };
 
-            storyboard.Begin();
-        }
-        else
-        {
-            sliding.Width = e.ActualWidth;
-            sliding.Height = e.ActualHeight;
-            sliding.RenderTransform = new TranslateTransform(offset.X, offset.Y);
-        }
-    }
+		AddStoryboard(storyboard, easingFuction, position.ActualWidth, "Width");
+		AddStoryboard(storyboard, easingFuction, position.ActualHeight, "Height");
+		AddStoryboard(
+			storyboard,
+			easingFuction,
+			position.OffSetX,
+			"(UIElement.RenderTransform).(TranslateTransform.X)"
+		);
+		AddStoryboard(
+			storyboard,
+			easingFuction,
+			position.OffSetY,
+			"(UIElement.RenderTransform).(TranslateTransform.Y)"
+		);
 
-    private void AddStoryboard(
-        Storyboard storyboard,
-        PowerEase powerEase,
-        double toValue,
-        string path
-    )
-    {
-        var animation = new DoubleAnimation(toValue, new Duration(TimeSpan.FromMilliseconds(200)))
-        {
-            EasingFunction = powerEase
-        };
-        Storyboard.SetTarget(animation, sliding);
-        Storyboard.SetTargetProperty(animation, new PropertyPath(path));
-        storyboard.Children.Add(animation);
-    }
+		storyboard.Begin();
+	}
 
-    private IEnumerable<T> GetChildren<T>()
-    {
-        return AssociatedObject switch
-        {
-            Panel panel => panel.Children.OfType<T>(),
-            ItemsControl itemsControl => itemsControl.Items.OfType<T>(),
-            _ => Enumerable.Empty<T>(),
-        };
-    }
+	private void AddStoryboard(
+		Storyboard storyboard,
+		PowerEase powerEase,
+		double toValue,
+		string path
+	)
+	{
+		var animation = new DoubleAnimation(toValue, new Duration(TimeSpan.FromMilliseconds(200)))
+		{
+			EasingFunction = powerEase
+		};
+		Storyboard.SetTarget(animation, sliding);
+		Storyboard.SetTargetProperty(animation, new PropertyPath(path));
+		storyboard.Children.Add(animation);
+	}
+
+	private Position GetPosition(FrameworkElement e)
+	{
+		var offset = VisualTreeHelper.GetOffset(e);
+		return new Position(e.ActualWidth, e.ActualHeight, offset.X, offset.Y);
+	}
+
+	private IEnumerable<T> GetChildren<T>()
+	{
+		return AssociatedObject switch
+		{
+			Panel panel => panel.Children.OfType<T>(),
+			ItemsControl itemsControl => itemsControl.Items.OfType<T>(),
+			_ => Enumerable.Empty<T>(),
+		};
+	}
 }
+
+internal record struct Position(double ActualWidth, double ActualHeight, double OffSetX, double OffSetY);
